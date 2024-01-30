9864399b5c7a6e90207584bd259376ef7c324121
Mankrik69 <mankrik69@gmail.com>
2024-01-31 01:43:37 +0700

    Create operate() function

--- index.js
+++ index.js
@@ -14,13 +14,19 @@ function divide(a, b) {
     return a / b;
 }
 
-// function operate(operator, a, b) {
-//     switch (operator) {
-//         case "+":
-//             return add(a, b);
-//     }
-// }
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b)
+    }
+}
 
-// let firstNumber = 10;
-// let operator = "+";
-// let secondNumber = 20;
+let firstNumber = 10;
+let operator = "+";
+let secondNumber = 20;
