0d37a9e450b97f9de60fc569c4d3b3d18dbd9444
Mankrik69 <mankrik69@gmail.com>
2024-01-31 01:41:37 +0700

    Create basic math operations

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// function operate(operator, a, b) {
+//     switch (operator) {
+//         case "+":
+//             return add(a, b);
+//     }
+// }
+
+// let firstNumber = 10;
+// let operator = "+";
+// let secondNumber = 20;
